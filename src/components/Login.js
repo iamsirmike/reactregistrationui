719263f6f88566b265c28eadb477b349a7a2570c
iamsirmike <asamoahmichael77@gmail.com>
2019-10-02 22:52:39 +0000

    added login modal

--- src/components/Login.js
+++ src/components/Login.js
@@ -18,13 +18,58 @@ class Login extends Component{
         }));
       }
     render(){
+      //styling
+      const input ={
+        width: "100%",
+        padding: "12px 20px",
+        margin: "2px 0",
+        border: "1px solid ##a6a6a6",
+        borderTopColor: "#949494",
+        borderRadius: "5px",
+        display: "inline-block"
+        
+    };
+    const form2 ={
+      padding: "20px",
+      // borderRadius: "400px !mportant"
+  };
+  const button={
+      backgroundColor: "blueviolet",
+      width: "100%"
+  };
+  const seeU={
+    color: "blueviolet"
+  }
+
+    //end styling
         return(
             <div>
             <center style={{color: "blueviolet"}}> <p onClick={this.toggle} id="btn">Already have an account? LOGIN</p></center>
                 <Modal isOpen={this.state.modal} toggle={this.toggle}>
-                <ModalHeader toggle={this.toggle}></ModalHeader>
+                {/* <ModalHeader toggle={this.toggle}></ModalHeader> */}
                 <ModalBody>
-                    //insert all codes here
+                    <Container>
+                        <Row>
+                         <Col>
+                          <div className="form2" style={form2}>
+                            <center><h3>Login</h3></center>
+                            <Form>
+                              <FormGroup>
+                                <Input type="email" id="email" className="email" placeholder="Email" style={input}/> 
+                              </FormGroup>
+                              <FormGroup>
+                                <Input type="passord" id="password" className="password" placeholder="Password" style={input}/> 
+                              </FormGroup>
+                              <FormGroup>
+                                    <Button style={button}>Login</Button>
+                              </FormGroup>
+                              <center style={{color: "#000000"}}>If you don't have an account click outside this box to close it and register.<br /> <br /></center><hr />
+                              <center style={seeU}>SEE YOU SOON</center>
+                          </Form>
+                          </div>
+                         </Col>
+                       </Row>
+                    </Container>
                 </ModalBody>
                 </Modal>
             </div>
